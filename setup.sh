55a43eba6d0fdc1bd5716316c695a46cba8969b6
JesonWu00 <152182212+JesonWu00@users.noreply.github.com>
2023-12-18 21:55:52 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"wuyl23offer@126.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
